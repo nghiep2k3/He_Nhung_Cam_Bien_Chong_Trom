6322ce8e21ddd59702333fcd9ece72a598050401
Nguyễn Nghiệp <nghiep2k3>
2024-10-16 23:46:12 +0700

    env

--- backend/cuoiki.py
+++ backend/cuoiki.py
@@ -30,8 +30,8 @@ def play_alarm():
     pygame.mixer.music.play()
 
 def send_sms():
-    #Thêm token vào đây
-    
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
